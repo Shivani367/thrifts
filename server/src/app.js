d68970f0c0ca5e480974f9fb55b4b16c3328d812
Shivani M <m.shivaniii017@gmail.com>
2026-06-12 13:10:57 +0530

    Implemented user registration API

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,31 @@
-const express=require("express");
-const cors=require("cors");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
-
-
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const User = require("./models/User");
 
-const app=express();
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.json({
-        success:true, 
-        message:"Trifts API is running"
-    });
-    
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Thrifts API is running",
+  });
 });
 
-const PORT=process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
+
 connectDB();
 
-const User = require("./models/User");
-
 console.log(User.modelName);
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
