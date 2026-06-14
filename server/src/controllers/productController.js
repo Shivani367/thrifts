a70c31be20c7b0f96abd6866e5527cebf2db1228
Shivani M <m.shivaniii017@gmail.com>
2026-06-14 14:38:43 +0530

    Added product search and category filtering

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -57,10 +57,27 @@ const product = await Product.create({
     });
   }
 };
+
 const getProducts = async (req, res) => {
   try {
-    const products = await Product.find()
-.populate("seller", "name email");
+    const keyword = req.query.keyword
+  ? {
+      title: {
+        $regex: req.query.keyword,
+        $options: "i",
+      },
+    }
+  : {};
+
+const category = req.query.category
+  ? { category: req.query.category }
+  : {};
+
+    const products = await Product.find({
+  ...keyword,
+  ...category,
+})
+      .populate("seller", "name email");
 
     res.status(200).json({
       success: true,
@@ -74,6 +91,7 @@ const getProducts = async (req, res) => {
     });
   }
 };
+
 const getProductById = async (req, res) => {
   try {
     const product = await Product.findById(
