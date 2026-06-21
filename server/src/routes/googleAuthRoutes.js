8af2aea1ec5aabcd6818b7d7f865d4e5c4ffadb0
Shivani M <m.shivaniii017@gmail.com>
2026-06-21 18:29:51 +0530

    Fix Google callback route

--- server/src/routes/googleAuthRoutes.js
+++ server/src/routes/googleAuthRoutes.js
@@ -1,4 +1,3 @@
-console.log("HEllo");
 const express = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
@@ -8,16 +7,35 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.send("Google Route Works");
 });
-router.get("/test", (req, res) => {
-  res.send("Google Auth Route Working");
-});
 
 router.get(
   "/google",
   passport.authenticate("google", {
     scope: ["profile", "email"],
   })
-  
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/login",
+  }),
+  async (req, res) => {
+    const token = jwt.sign(
+      {
+        id: req.user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.redirect(
+      `${process.env.FRONTEND_URL}/oauth-success?token=${token}`
+    );
+  }
 );
 
 module.exports = router;
